a5de083de1ff0a160f21d5dd6df9e6424fbcdd01
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2020-05-30 11:07:39 +0530

    basic tests for parsing

---
+++ tests/test_parse.py
@@ -0,0 +1,38 @@
+from pulseplot import *
+
+def test_parse_base_1():
+    seq = r"p1 pl1 ph1 ch2"
+    out = parse_base(seq)
+    print(out)
+    
+    for k, v in PARAMS.items():
+        assert v[0] in out.keys()
+
+    assert out["plen"] == 1.0
+    assert out["power"] == 1.0
+    assert out["phase"] == "1"
+    assert out["channel"] == 2.0
+    assert out["time"] is None
+
+
+def test_parse_base_2():
+    seq = r"d1 ch1 tx$\\tau$"
+    out = parse_base(seq)
+    
+    for k, v in PARAMS.items():
+        assert v[0] in out.keys()
+
+    assert out["plen"] is None
+    assert out["power"] is None
+    assert out["text"] == r"$\\tau$"
+
+
+def test_parse_single():
+    
+    seq = r"ph2 tx2 ch2"
+    out = parse_single(seq)
+    print(out)
+    
+
+if __name__ == "__main__":
+    test_parse_single()
