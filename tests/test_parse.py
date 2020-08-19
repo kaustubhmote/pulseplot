3a907d99f512b0e80e5acbb0b06b9d7d402fb60a
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2020-08-19 10:30:34 +0530

    added new tests, moved all strings to a separate object

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,12 +1,25 @@
 from pulseplot import *
 
-def test_parse_base_1():
-    seq = r"p1 pl1 ph1 ch2"
-    out = parse_base(seq)
-    
-    for k, v in PARAMS.items():
-        assert v[0] in out.keys()
 
+test_string = r"""p1 pl1 ph1 ch2
+d1 ch1 tx$\\tau$
+d1 ch1 txTEST tdx0.1 tdy0.3
+p1 pl1 ph1 ch8 fcr ecg al0.33
+p3 pl4 ph_x
+p2.6 pl5 ph2 phpdx0.1 phpdy0.2
+""".split("\n")
+
+
+
+def _check_allowed_parameters(out):
+
+    assert set([v.name for v in PARAMS.values()]) == set(out.keys())
+
+
+def test_parse_base_1():
+    out = parse_base(test_string[0])
+
+    _check_allowed_parameters(out)
     assert out["plen"] == 1.0
     assert out["power"] == 1.0
     assert out["phase"] == "1"
@@ -15,23 +28,35 @@ def test_parse_base_1():
 
 
 def test_parse_base_2():
-    seq = r"d1 ch1 tx$\\tau$"
-    out = parse_base(seq)
-    
-    for k, v in PARAMS.items():
-        assert v[0] in out.keys()
+    out = parse_base(test_string[1])
 
+    _check_allowed_parameters(out)
     assert out["plen"] is None
     assert out["power"] is None
     assert out["text"] == r"$\\tau$"
+    assert out["time"] == 1.0
 
 
-def test_parse_single():
-    seq = r"ph2 tx2 ch2"
-    out = parse_base(seq)
-    assert out["phase"] == "2"
-    assert out["text"] == "2"
-    assert out["channel"] == 2.0
+def test_parse_base_3():
+    out = parse_base(test_string[2])
+
+    _check_allowed_parameters(out)
+    assert out["plen"] is None
+    assert out["time"] == 1.0
+    assert out["channel"] == 1
+    assert out["text"] == "TEST"
+    assert out["text_dx"] == 0.1
+    assert out["text_dy"] == 0.3
+    
+
+def test_parse_base_4():
+    out = parse_base(test_string[4])
+
+    _check_allowed_parameters(out)
+    assert out["plen"] == 3.0
+    assert out["power"] == 4.0
+    assert out["phase"] == "_x"
+
 
 
 def test_shape():
@@ -52,8 +77,10 @@ def test_userparams():
     assert out["plen"] == 1.0
     assert out["power"] == 2.0
     assert callable(out["shape"])
+    assert out["shape"](100) == 101
     assert out["channel"] == 0.0
     
 
 if __name__ == "__main__":
-    test_shape()
+    
+    print(test_string[0])
