8c440bba5e3f790535fac2749d8dc155d86c4960
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2020-12-30 23:01:16 +0530

    added test for pulseseq

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,9 +1,12 @@
+from pulseplot.parse import PulseSeq, Pulse
 import matplotlib.pyplot as plt
 import numpy as np
 from pulseplot import pplot
 from pathlib import Path
+import time
 
 TESTDIR = Path(__file__).parent
+
 
 def test_pulse_1():
     fig, ax = pplot()
@@ -23,3 +26,59 @@ def test_pulse_1():
     ax.set_ylim(0, 3)
 
     fig.savefig(TESTDIR.joinpath("test_pulse_1.png"))
+
+
+def test_pulse_delay():
+    fig, ax = pplot()
+    ax.pulse(r"p1 pl1 ph1 f1 fck")
+    ax.delay(r"d2 tx$\tau$ f1")
+    ax.pulse(r"p1 pl1 ph1 f1 fck")
+    ax.delay(r"d2 tx$\tau$ f1")
+    ax.fid(r"p2 pl0.5 f1")
+    ax.set_xlim(0, 11)
+    ax.set_ylim(0, 3)
+    fig.savefig(TESTDIR.joinpath("test_pulse_delay.png"))
+
+
+def test_pulseseq():
+
+    p = r"""
+    p1 pl1 ph_x f2 fck nH90
+    d2 tx$\tau$ f1
+    p2 pl1 ph2 f2 w
+    p2 pl1 ph3 f1 
+    d2 tx$\tau$ f1
+    p1 pl1 ph4 f1 fck w
+    p1 pl1 ph5 f2 fck
+    p3 pl1 spf f1 troff o fcnone ecr w
+    p3 pl0.9 sp=rampup_30 f2 tx=decoupling tkw={'fontsize':10} tdy=-0.1
+    """
+
+
+    fig, ax = pplot()
+    ax.params = {"p1": 0.2, "pl1": 1, "p2": 0.3, "f1": 0, "f2": 2}
+    p = PulseSeq(p, external_params=ax.params)
+    p.edit(name="H90", facecolor="r")
+    ax.spacing = 0.05
+    ax.phase_dy = 0.2
+    ax.fontsize = 15
+    # ax.text_dy = 1
+    ax.pseq(p)
+    ax.set_xlim(-1, 15)
+    ax.set_ylim(-1, 5)
+    fig.savefig(TESTDIR.joinpath("test_pulseseq.png"))
+    ax.clear()
+    ax.reset()
+    p.edit(index=0, phase=r"_y")
+    ax.pseq(p)
+
+    ax.set_xlim(-1, 15)
+    ax.set_ylim(-1, 5)
+
+    print(ax.texts[1].remove())
+
+    fig.savefig(TESTDIR.joinpath("test_pulseseq.png"))
+
+
+if __name__ == "__main__":
+    test_pulseseq()
